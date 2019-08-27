e7ee30137223a86d69f4e3af6c5e9af153ac1551
李柏成 <729127678@qq.com>
2019-08-27 17:27:36 +0800

    1

--- application/api/controller/Ranking.php
+++ application/api/controller/Ranking.php
@@ -29,6 +29,9 @@ class Ranking extends ApiBase
             $this->ajaxReturn(['status' => -2 , 'msg'=>'开奖时间段，不能下单，请等待'.$end_time-time().'秒']);
         }
         $num=I('num',1);
+        if($num<1){
+            $this->ajaxReturn(['status' => -2 , 'msg'=>'请输入正确的购买数量']);
+        }
         $RankingLogic = new RankingLogic();
         $res = $RankingLogic->buy_gold_shovel($user_id,$num);
         $this->ajaxReturn($res);
