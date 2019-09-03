2082ac97cbd35f13df7b1e83a01cd8e131040241
李柏成 <729127678@qq.com>
2019-09-03 16:17:37 +0800

    提现说明修改

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -391,6 +391,7 @@ class User extends ApiBase
             'rate' => $rate,
             'fee' => $taxfee,
             'money' => $money,
+            'actual_money'=>($money-$taxfee),
             'status' => 0,
             'create_time' => time(),
         ]);
