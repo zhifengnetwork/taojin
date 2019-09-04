b9d6532d2df1e19abc55b16c4dd43e537c9c0cc3
李柏成 <729127678@qq.com>
2019-09-04 18:08:35 +0800

    后台订单导出

--- application/admin/controller/Ranking.php
+++ application/admin/controller/Ranking.php
@@ -12,6 +12,19 @@ class Ranking extends Common
             $keyword = input('post.key');
             $page = input('page') ? input('page') : 1;
             $pageSize = input('limit') ? input('limit') : config('pageSize');
+            $start_time=input('post.start_time');
+            $end_time=input('post.end_time');
+            if(!empty($start_time)){
+                if(!empty($start_time)){
+                    if(strtotime($start_time)>strtotime($end_time)){
+                        $map['rank_time']=['between',[strtotime($start_time),time()]];
+                    }else{
+                        $map['rank_time']=['between',[strtotime($start_time),strtotime($end_time)]];
+                    }
+                }else{
+                    $map['rank_time']=['between',[strtotime($start_time),time()]];
+                }
+            }
 
             if(!empty($keyword)){
                 $map['id'] = array('like','%' . $keyword . '%');
@@ -23,7 +36,7 @@ class Ranking extends Common
                 ->order('id desc')
                 ->paginate(array('list_rows' => $pageSize,'page' => $page))
                 ->toArray();
-
+//
             return [
                 'code' => 0,
                 'msg' => '获取成功',
@@ -63,6 +76,120 @@ class Ranking extends Common
             return ['code'=>0,'msg' => '排序失败！','url' => url('index')];
         }
     }
+
+    /**
+     * 导出
+     * @throws \PHPExcel_Exception
+     * @throws \PHPExcel_Writer_Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+
+    function exportExcel_old(){
+        $keyword=input('key');
+        $start_time=input('start_time');
+        $end_time=input('end_time');
+
+        $where=[];
+        if(!empty($start_time)){
+            if(!empty($start_time)){
+                if(strtotime($start_time)>strtotime($end_time)){
+                    $where['rank_time']=['between',[strtotime($start_time),time()]];
+                }else{
+                    $where['rank_time']=['between',[strtotime($start_time),strtotime($end_time)]];
+                }
+            }else{
+                $where['rank_time']=['between',[strtotime($start_time),time()]];
+            }
+        }
+
+        if(!empty($keyword)){
+            $where['id'] = array('like','%' . $keyword . '%');
+        }
+
+        $count=Db::name('ranking')->field('id,user_id,rank_time,add_time,rank_status')->where($where)->count();;
+        if($count>20000){
+            echo '数据量过大，不能导出！数量：'.$count;
+            die;
+        }
+        $res=Db::name('ranking')->field('id,user_id,rank_time,add_time,rank_status')->where($where)->select();
+        error_reporting(0);
+        vendor('PHPExcel.PHPExcel');
+
+        //导入Excel文件
+        $objPHPExcel = new \PHPExcel();
+        $objPHPExcel->setActiveSheetIndex(0);
+        // 设置sheet名
+        $objPHPExcel->getActiveSheet()->setTitle('xx列表');
+
+        // 设置表格宽度
+        $objPHPExcel->getActiveSheet()->getColumnDimension('A')->setWidth(10);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(20);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(15);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('D')->setWidth(50);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('E')->setWidth(20);
+//        $objPHPExcel->getActiveSheet()->getColumnDimension('F')->setWidth(5);
+//        $objPHPExcel->getActiveSheet()->getColumnDimension('G')->setWidth(20);
+
+
+        // 列名表头文字加粗
+        $objPHPExcel->getActiveSheet()->getStyle('A1:J1')->getFont()->setBold(true);
+        // 列表头文字居中
+        $objPHPExcel->getActiveSheet()->getStyle('A1:J1')->getAlignment()
+            ->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+
+        // 列名赋值
+        $objPHPExcel->getActiveSheet()->setCellValue('A1', '编号');
+        $objPHPExcel->getActiveSheet()->setCellValue('B1', '用户id');
+        $objPHPExcel->getActiveSheet()->setCellValue('C1', '排位状态');
+        $objPHPExcel->getActiveSheet()->setCellValue('D1', '排位时间');
+        $objPHPExcel->getActiveSheet()->setCellValue('E1', '下单时间');
+//        $objPHPExcel->getActiveSheet()->setCellValue('F1', '审核');
+//        $objPHPExcel->getActiveSheet()->setCellValue('G1', '审核时间');
+
+        // 数据起始行
+        $row_num = 2;
+        // 向每行单元格插入数据
+        foreach($res as $value)
+        {
+            // 设置所有垂直居中
+            $objPHPExcel->getActiveSheet()->getStyle('A' . $row_num . ':' . 'J' . $row_num)->getAlignment()
+                ->setVertical(\PHPExcel_Style_Alignment::VERTICAL_CENTER);
+            // 设置价格为数字格式
+            $objPHPExcel->getActiveSheet()->getStyle('D' . $row_num)->getNumberFormat()
+                ->setFormatCode(\PHPExcel_Style_NumberFormat::FORMAT_NUMBER_00);
+            // 居中
+            $objPHPExcel->getActiveSheet()->getStyle('E' . $row_num . ':' . 'H' . $row_num)->getAlignment()
+                ->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+
+            // 设置单元格数值
+            $objPHPExcel->getActiveSheet()->setCellValue('A' . $row_num, $value['id']);
+            $objPHPExcel->getActiveSheet()->setCellValue('B' . $row_num, $value['user_id']);
+            $objPHPExcel->getActiveSheet()->setCellValue('C' . $row_num, $value['rank_status'] ? '出局' : '未出局');
+            $objPHPExcel->getActiveSheet()->setCellValue('D' . $row_num, date('Y-m-d h:i:s',$value['rank_time']));
+            $objPHPExcel->getActiveSheet()->setCellValue('E' . $row_num, date('Y-m-d h:i:s',$value['add_time']));
+//            $objPHPExcel->getActiveSheet()->setCellValue('F' . $row_num, $value['state'] ? '√' : '×');
+//            $objPHPExcel->getActiveSheet()->setCellValue('G' . $row_num, date('Y-m-d h:i:s',$value['statetime']));
+            $row_num++;
+        }
+
+        $outputFileName = '排位订单' . time() . '.xls';
+        $xlsWriter = new \PHPExcel_Writer_Excel5($objPHPExcel);
+        header("Content-Type: application/force-download");
+        header("Content-Type: application/octet-stream");
+        header("Content-Type: application/download");
+        header('Content-Disposition:inline;filename="' . $outputFileName . '"');
+        header("Content-Transfer-Encoding: binary");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        header("Pragma: no-cache");
+        $xlsWriter->save("php://output");
+        exit();
+    }
+
+
     /*
      * 包时间段设置
      */
