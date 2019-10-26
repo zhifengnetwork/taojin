642b3cd75afde8dc396dd0bc03bbc0e08fe11580
李柏成 <729127678@qq.com>
2019-10-26 16:16:18 +0800

    开启测试

--- application/api/controller/Egg.php
+++ application/api/controller/Egg.php
@@ -161,8 +161,8 @@ class Egg extends ApiBase
         }
     }
     public function get_time(){
-        $one_time=strtotime(date("Y-m-d")." 12:00:00");
-        $two_time=strtotime(date("Y-m-d")." 13:00:00");
+        $one_time=strtotime(date("Y-m-d")." 16:00:00");
+        $two_time=strtotime(date("Y-m-d")." 18:00:00");
         $three_time=strtotime(date("Y-m-d")." 18:00:00");
         $four_time=strtotime(date("Y-m-d")." 19:00:00");
         $data['one_time']=$one_time;
